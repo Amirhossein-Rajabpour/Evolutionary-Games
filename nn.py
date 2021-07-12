75ed8e9471458de6fdc7fcfe09a0a520e077bd9c
Amirhossein Rajabpour <rajabpouramirhosein@gmail.com>
2021-07-12 13:57:37 +0430

    sigmoid activation function added and shaping bugs solved

--- nn.py
+++ nn.py
@@ -15,16 +15,20 @@ class NeuralNetwork():
         self.W_2 = np.random.randn(self.hidden_neurons_num, self.output_neurons_num)
         self.b_2 = np.zeros((self.output_neurons_num, 1))
 
-    def activation(self, x):    # Leaky ReLU activation function
-        
-        x = np.where(x > 0, x, x * 0.01)
+    def activation(self, x, activation_type):    # Leaky ReLU activation function
+
+        if activation_type == 'LRelu':
+            x = np.where(x > 0, x, x * 0.01)
+        elif activation_type == 'sigmoid':
+            x = 1/(1 + np.exp(-x))
         return x
 
     def forward(self, x):
         
         # x example: np.array([[0.1], [0.2], [0.3]])
+        x = x.reshape(x.shape[0], 1)
         z_1 = np.matmul(self.W_1.T, x) + self.b_1
-        a_1 = self.activation(z_1)
+        a_1 = self.activation(z_1, activation_type='LRelu')
         z_2 = np.matmul(self.W_2.T, a_1) + self.b_2
-        output = self.activation(z_2)
+        output = self.activation(z_2, activation_type='sigmoid')
         return output
